54478a4fc00c790ad2fb22dee03e1b8975b1f111
João Costa <worldoftanksreviewer@gmail.com>
2023-03-30 17:40:59 +0100

    Separated code into main loop and helper functions util.py

--- circleTest.py
+++ circleTest.py
@@ -2,178 +2,9 @@ import array
 import sys
 import cv2 as cv
 import numpy as np
-from math import ceil 
-def getCenter(points):
-    xAcc = 0
-    yAcc = 0
-    for point in points:
-        xAcc += point[0][0]
-        yAcc += point[0][1]
-    
-    count = len(points)
+from math import ceil, atan2
+import util as u
 
-    return (xAcc // count, yAcc // count)
-
-def extendPoint(finderPattern, center, magnitude):
-    vx = finderPattern[0] - center[0]
-    vy = finderPattern[1] - center[1]
-    return (int(center[0] + vx * magnitude), int(center[1] + vy * magnitude))
-
-def checkRatio(stateCount):
-    totalSize = 0
-    for count in stateCount:
-        totalSize += count
-        if count == 0:
-            return False
-    if totalSize < 7:
-        return False
-
-    moduleSize = ceil(totalSize / 7.0)
-    maxVariance = moduleSize // 2
-
-    val = ((abs(moduleSize - (stateCount[0])) < maxVariance) and
-        (abs(moduleSize - (stateCount[1])) < maxVariance) and
-        (abs(2.5*moduleSize - (stateCount[2])) < 2.5*maxVariance) and
-        (abs(moduleSize - (stateCount[3])) < maxVariance) and
-        (abs(moduleSize - (stateCount[4])) < maxVariance))
-    
-    return val
-
-def centerFromEnd(stateCount, end):
-    return end - stateCount[4] - stateCount[3] - (stateCount[2] / 2.0)
-
-def verifyHorizontal(gray, center):
-    rows = gray.shape[0]
-    cols = gray.shape[1]
-    stateCount = [0, 0, 0, 0, 0]
-
-    # Paint original center
-    # image[center[1]][center[0]] = (0, 0, 255)
-    if(center[1] >= rows):
-        return None
-    row = gray[center[1]]
-    col = center[0]
-    
-    ##################
-    # Traverse to the left
-
-    # Still in black center (state 2)
-    while(col >= 0 and row[col] < 128):
-        stateCount[2] += 1
-        col -= 1
-
-    if(col < 0):
-        return None
-
-    while(col >= 0 and row[col] > 128):
-        stateCount[1] += 1
-        col -= 1
-
-    if(col < 0):
-        return None
-
-    while(col >= 0 and row[col] < 128):
-        stateCount[0] += 1
-        col -= 1
-    
-    if(col < 0):
-        return None
-
-    #############
-    # Traverse to the right
-    col = center[0]
-    while(col < cols and row[col] < 128):
-        stateCount[2] += 1
-        col += 1
-    if(col == cols):
-        return None
-    while(col < cols and row[col] > 128):
-        stateCount[3] += 1
-        col += 1
-    if(col == cols):
-        return None
-    while(col < cols and row[col] < 128):
-        stateCount[4] += 1
-        col += 1
-
-    if(col == cols):
-        return None
-
-    if(checkRatio(stateCount)):        
-        newCenter = ceil(centerFromEnd(stateCount, col))
-        # image[center[1]][newCenter] = (255, 255, 255)
-        return (newCenter, center[1])
-        #return (newCenter, center[1])
-    else:
-        return None    
-
-def verifyVertical(gray, center):
-    rows = gray.shape[0]
-    cols = gray.shape[1]
-    stateCount = [0, 0, 0, 0, 0]
-
-    # Paint original center
-    # image[center[1]][center[0]] = (0, 0, 255)
-    
-    row = center[1]
-    col = center[0]
-    
-    ##################
-    # Traverse down
-
-    # Still in black center (state 2)
-    while(row >= 0 and gray[row][col] < 128):
-        stateCount[2] += 1
-        row -= 1
-
-    if(row < 0):
-        return None
-
-    while(row >= 0 and gray[row][col] > 128):
-        stateCount[1] += 1
-        row -= 1
-
-    if(row < 0):
-        return None
-
-    while(row >= 0 and gray[row][col] < 128):
-        stateCount[0] += 1
-        row -= 1
-    
-    if(row < 0):
-        return None
-
-    #############
-    # Traverse up
-    row = center[1]
-    while(row < rows and gray[row][col] < 128):
-        stateCount[2] += 1
-        row += 1
-    if(row == rows):
-        return None
-    while(row < rows and gray[row][col] > 128):
-        stateCount[3] += 1
-        row += 1
-    if(row == rows):
-        return None
-    while(row < rows and gray[row][col] < 128):
-        stateCount[4] += 1
-        row += 1
-
-    if(row == rows):
-        return None
-
-    if(checkRatio(stateCount)):        
-        newCenter = ceil(centerFromEnd(stateCount, row))
-        
-        totalSize = 0
-        for state in stateCount:
-            totalSize += state
-        # image[center[1]][newCenter] = (255, 255, 255)
-        #return (newCenter, center[0])
-        return ((center[0], newCenter), totalSize)
-    else:
-        return None 
 
 cap = cv.VideoCapture(0)
 paused = False
@@ -184,64 +15,69 @@ while(True):
     if not paused:
         src = cap.read()[1]
         gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-        thres = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 51, 0)
-
-        gray = thres
+        gray = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 51, 0)
         gray = cv.medianBlur(gray, 5)
         
-
         rows = gray.shape[0]
         circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
                                     param1=100, param2=30,
                                     minRadius=1, maxRadius=60)
 
-
-    if circles is not None and circles.size >= 3 * 3:
-        if(cv.waitKey(1) == 9) and not paused:
-            paused = True
-            for c in circles[0, :]:
-                    possibleCenters.append((c[0], c[1]))
-
     if not paused:
-        if circles is not None:
+        if circles is not None and circles.size / 3 >= 4:
             totalCircles = circles.size / 3
             circles = np.uint16(np.around(circles))
             finderPatterns = []
+
+            # Check if found circles are finder patterns
             for i in circles[0, :]:
+
+                #i -> (x, y, radius)
                 center = (i[0], i[1])
                 
-                horCenter = verifyHorizontal(gray, center)
+                # Horizontally aligned center
+                # (x, y)
+                horCenter = u.alignHorizontal(gray, center)
 
                 if horCenter is None:
                     totalCircles -= 1
                     continue
 
-                newCenter = verifyVertical(gray, horCenter)
+                # Vertically aligned center and diameter
+                # (x, y, pattern diameter)
+                patFinder = u.alignVertical(gray, horCenter)
 
-                if newCenter is None:
+                if patFinder is None:
                     totalCircles -= 1
                     continue
 
-                # circle center
+                # Old circle center and circumference
                 cv.circle(src, center, 1, (0, 0, 255), 3)
-                # circle outline
-                radius = i[2]
-                cv.circle(src, center, radius, (0, 0, 255), 3)
-                finderPatterns.append(newCenter)
-                # circle new center
-                cv.circle(src, newCenter[0], 1, (0, 255, 0), 3)
-                cv.circle(src, newCenter[0], newCenter[1]//2, (0, 255, 0), 3)
-            cv.putText(src, f"Valid circles: {totalCircles}/{circles.size/3}", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            if totalCircles == 4:
-                paused = True
-                sqrCenter = getCenter(finderPatterns)
+                cv.circle(src, center, i[2], (0, 0, 255), 3)
 
-                cv.line(src, finderPatterns[3][0], finderPatterns[0][0], (0, 0, 255), 3)
+                finderPatterns.append(patFinder[0])
+
+                # Circle aligned center
+                cv.circle(src, patFinder[0], 1, (0, 255, 0), 3)
+                cv.circle(src, patFinder[0], patFinder[1]//2, (0, 255, 0), 3)
+
+            cv.putText(src, f"Valid circles: {totalCircles}/{circles.size/3}", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            
+            if totalCircles == 4:
+                # (x, y)
+                sqrCenter = u.getCenter(finderPatterns)
+                corners = []
                 for i in range(0, len(finderPatterns), 1):
-                    newPos = extendPoint(finderPatterns[i][0], sqrCenter, 1.75)
-                    cv.circle(src, newPos, 15, (0, 0, 255), 3)
-                    # cv.line(src, finderPatterns[i][0], finderPatterns[i+1][0], (0, 0, 255), 3)
-                cv.circle(src, sqrCenter, 15, (0, 0, 255), 3)
+                    newPos = u.extendPoint(finderPatterns[i], sqrCenter, 1.75)
+                    if not u.checkBounds(newPos, src):
+                        break
+                        
+                    corners.append(newPos)
+                if len(corners) == 4:
+                    paused = True
+                    for corner in corners:
+                        cv.circle(src, corner, 5, (0, 0, 255), 3)
+
 
 
         cv.imshow("detected circles", src)
---
+++ util.py
@@ -0,0 +1,188 @@
+from math import ceil
+
+# Returns center of list of FinderPattern (x, y)
+def getCenter(points):
+    xAcc = 0
+    yAcc = 0
+    for point in points:
+        xAcc += point[0]
+        yAcc += point[1]
+    
+    count = len(points)
+
+    return (xAcc // count, yAcc // count)
+
+# Returns new point (x, y)
+def extendPoint(finderPattern, center, magnitude):
+    vx = finderPattern[0] - center[0]
+    vy = finderPattern[1] - center[1]
+    return (int(center[0] + vx * magnitude), int(center[1] + vy * magnitude))
+
+
+def checkBounds(point, image):
+    if point[0] <= 0 or point[1] <= 0:
+        return False
+    
+    if point[0] >= image.shape[1]:
+        return False
+
+    if point[1] >= image.shape[0]:
+        return False
+
+    return True
+
+def checkRatio(stateCount):
+    totalSize = 0
+    for count in stateCount:
+        totalSize += count
+        if count == 0:
+            return False
+    if totalSize < 7:
+        return False
+
+    moduleSize = ceil(totalSize / 7.0)
+    maxVariance = moduleSize // 2
+
+    val = ((abs(moduleSize - (stateCount[0])) < maxVariance) and
+        (abs(moduleSize - (stateCount[1])) < maxVariance) and
+        (abs(2.5*moduleSize - (stateCount[2])) < 2.5*maxVariance) and
+        (abs(moduleSize - (stateCount[3])) < maxVariance) and
+        (abs(moduleSize - (stateCount[4])) < maxVariance))
+    
+    return val
+
+def centerFromEnd(stateCount, end):
+    return end - stateCount[4] - stateCount[3] - (stateCount[2] / 2.0)
+
+def alignHorizontal(gray, center):
+    rows = gray.shape[0]
+    cols = gray.shape[1]
+    stateCount = [0, 0, 0, 0, 0]
+
+    # Paint original center
+    # image[center[1]][center[0]] = (0, 0, 255)
+    if(center[1] >= rows):
+        return None
+    row = gray[center[1]]
+    col = center[0]
+    
+    ##################
+    # Traverse to the left
+
+    # Still in black center (state 2)
+    while(col >= 0 and row[col] < 128):
+        stateCount[2] += 1
+        col -= 1
+
+    if(col < 0):
+        return None
+
+    while(col >= 0 and row[col] > 128):
+        stateCount[1] += 1
+        col -= 1
+
+    if(col < 0):
+        return None
+
+    while(col >= 0 and row[col] < 128):
+        stateCount[0] += 1
+        col -= 1
+    
+    if(col < 0):
+        return None
+
+    #############
+    # Traverse to the right
+    col = center[0]
+    while(col < cols and row[col] < 128):
+        stateCount[2] += 1
+        col += 1
+    if(col == cols):
+        return None
+    while(col < cols and row[col] > 128):
+        stateCount[3] += 1
+        col += 1
+    if(col == cols):
+        return None
+    while(col < cols and row[col] < 128):
+        stateCount[4] += 1
+        col += 1
+
+    if(col == cols):
+        return None
+
+    if(checkRatio(stateCount)):        
+        newCenter = ceil(centerFromEnd(stateCount, col))
+        # image[center[1]][newCenter] = (255, 255, 255)
+        return (newCenter, center[1])
+        #return (newCenter, center[1])
+    else:
+        return None    
+
+def alignVertical(gray, center):
+    rows = gray.shape[0]
+    cols = gray.shape[1]
+    stateCount = [0, 0, 0, 0, 0]
+
+    # Paint original center
+    # image[center[1]][center[0]] = (0, 0, 255)
+    
+    row = center[1]
+    col = center[0]
+    
+    ##################
+    # Traverse down
+
+    # Still in black center (state 2)
+    while(row >= 0 and gray[row][col] < 128):
+        stateCount[2] += 1
+        row -= 1
+
+    if(row < 0):
+        return None
+
+    while(row >= 0 and gray[row][col] > 128):
+        stateCount[1] += 1
+        row -= 1
+
+    if(row < 0):
+        return None
+
+    while(row >= 0 and gray[row][col] < 128):
+        stateCount[0] += 1
+        row -= 1
+    
+    if(row < 0):
+        return None
+
+    #############
+    # Traverse up
+    row = center[1]
+    while(row < rows and gray[row][col] < 128):
+        stateCount[2] += 1
+        row += 1
+    if(row == rows):
+        return None
+    while(row < rows and gray[row][col] > 128):
+        stateCount[3] += 1
+        row += 1
+    if(row == rows):
+        return None
+    while(row < rows and gray[row][col] < 128):
+        stateCount[4] += 1
+        row += 1
+
+    if(row == rows):
+        return None
+
+    if(checkRatio(stateCount)):        
+        newCenter = ceil(centerFromEnd(stateCount, row))
+        
+        totalSize = 0
+        for state in stateCount:
+            totalSize += state
+        # image[center[1]][newCenter] = (255, 255, 255)
+        #return (newCenter, center[0])
+        return ((center[0], newCenter), totalSize)
+    else:
+        return None 
