ded5ece55edc480eb06f158eee90205555ab4c9b
João Costa <worldoftanksreviewer@gmail.com>
2023-03-31 13:38:29 +0100

    Added data encoding into bits.

--- encode.py
+++ encode.py
@@ -4,51 +4,19 @@ import encodeUtil as en
 SIZE = 1000
 CENTER = (SIZE / 2, SIZE / 2)
 
-
-# Outputs array with values of R, one for each slice. 
-def splitIntoSlices(startR, endR, bitWidth):
-    amplitude = endR - startR
-    n = amplitude // bitWidth
-    stepSize = bitWidth / 2
-
-    slices = []
-    firstPos = startR + stepSize
-    slices.append(firstPos)
-    for i in range(n - 1):
-        firstPos += bitWidth
-        slices.append(firstPos)
-    return slices
-
-# Calculate N given the amplitude and radius
-# r must be absolute!!
-def calcN(amplitude, r, bitWidth):
-    slicePerimeter = (amplitude / 360) * (2 * pi * r)
-    n = slicePerimeter // bitWidth
-    return n
+accum = 0
 def drawRegion(angleStart, angleStop, startR, endR, width, color, img):
-    slices = splitIntoSlices(startR, endR, width)
+    global accum
+    slices = en.splitIntoSlices(startR, endR, width)
 
     angleAmplitude = angleStop - angleStart
     for slice in slices:
-        n = calcN(angleAmplitude, slice, width)
-        drawSector(angleStart, angleStop, slice, n, color, img)
+        n = en.calcN(angleAmplitude, slice, width)
+        en.drawSector(angleStart, angleStop, slice, n, color, img)
+        accum += n
 
 
-def drawSector(angleStart, angleStop, r, n, col, img):    
-    amplitude = angleStop - angleStart
-    areaPerim = (amplitude / 360) * (2*pi*r)
-    circleRadius = (areaPerim / n) / 2
-    step = amplitude / (2 * n)
-    angle = angleStart + step
-    #step = (r * 2 * 360) / ( 2 * pi * d *1000)
-    print(step) 
-    while angle < angleStop:
-        en.drawCircle(angle, r, circleRadius, col, img)
-        en.drawCircle(angle, r, 2, (0, 0, 0), img1)
-        angle += step * 2
-        global accum
-        accum +=1
-accum = 0
+
 #img = Image.new("RGB", (1000, 1000), color=(255, 255, 255))
 img = Image.open("uff.png", mode="r", formats=None)
 img1 = ImageDraw.Draw(img)
@@ -66,6 +34,8 @@ drawRegion(60, 122, 262, 365, 15, (255, 0, 0), img1)
 drawRegion(150, 212, 262, 365, 15, (255, 0, 0), img1)
 drawRegion(330, 392, 262, 365, 15, (255, 0, 0), img1)
 drawRegion(240, 302, 262, 365, 15, (255, 0, 0), img1)
+
+    
 shapeUp = [(500, 500), (500, 0)]
 shapeLeft = [(500, 500), (0, 500)]
 #img1.line(shapeUp, fill="red", width = 0)
--- encodeUtil.py
+++ encodeUtil.py
@@ -3,6 +3,23 @@ CENTER = (SIZE // 2, SIZE // 2)
 
 from math import cos, sin, pi
 
+DATA = [0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 
+0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 
+0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 
+0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 
+1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 
+1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 
+0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 
+1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 
+0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 
+0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 
+0, 1, 0, 0, 0, 1]
+
+INDEX = 0
+
+COLOR_YES = (255, 0, 0)
+COLOR_NO = (255, 255, 255)
+
 def circle(imgDraw, center, r, color):
     x = center[0]
     y = center[1]
@@ -16,8 +33,54 @@ def toCart(angle, dist, center):
 def toRad(angle):
     return (angle * -2*pi) / 360
 
-def drawCircle(angle, d, r, col, img):        
+def drawCircle(angle, d, r, col, img, enc):  
+    global INDEX, COLOR_NO, COLOR_YES, DATA      
     a = toRad(angle)
     dist = d
     c = toCart(a, dist, CENTER)
+    if INDEX < len(DATA) and enc:
+        bit = DATA[INDEX]
+        if bit == 0:
+            col = COLOR_NO
+        else:
+            col = COLOR_YES
+        INDEX += 1
     circle(img, c, r, col)
+    
+
+
+# Outputs array with values of R, one for each slice. 
+def splitIntoSlices(startR, endR, bitWidth):
+    amplitude = endR - startR
+    n = amplitude // bitWidth
+    stepSize = bitWidth / 2
+
+    slices = []
+    firstPos = startR + stepSize
+    slices.append(firstPos)
+    for i in range(n - 1):
+        firstPos += bitWidth
+        slices.append(firstPos)
+    return slices
+
+# Calculate N given the amplitude and radius
+# r must be absolute!!
+def calcN(amplitude, r, bitWidth):
+    slicePerimeter = (amplitude / 360) * (2 * pi * r)
+    n = slicePerimeter // bitWidth
+    return n
+
+
+def drawSector(angleStart, angleStop, r, n, col, img):    
+    amplitude = angleStop - angleStart
+    areaPerim = (amplitude / 360) * (2*pi*r)
+    circleRadius = (areaPerim / n) / 2
+    step = amplitude / (2 * n)
+    angle = angleStart + step
+    #step = (r * 2 * 360) / ( 2 * pi * d *1000)
+    print(step) 
+    while angle < angleStop:
+        drawCircle(angle, r, circleRadius, col, img, True)
+        drawCircle(angle, r, 2, (0, 0, 0), img, False)
+        angle += step * 2
+        
